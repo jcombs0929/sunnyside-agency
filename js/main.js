4ae88c1b7163da948fc053cd4d145d1a0bdf15a8
Jeff Combs <j.combs.design@gmail.com>
2022-02-22 10:55:20 -0600

    Hero: added responsive styles

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
-const mobileMenu = document.querySelector(".mobile-menu");
+const navMenu = document.querySelector(".menu");
 const heroText = document.querySelector(".hero-text");
+const heroImageDesktop = document.querySelector(".header__bkg--desktop");
+const heroImageMobile = document.querySelector(".header__bkg--mobile");
+
 let menu = false;
 
 window.onresize = reportWindowSize;
 
 function reportWindowSize() {
   let width = window.innerWidth;
-  //   if (width >= 768) {
-  //     toggleMenu();
-  //   }
+  if (width >= 768) {
+    toggleResponsive();
+  } else {
+    mobileMenuBtn.style.display = "block";
+  }
 }
 
 function onMenuToggle() {
   menu = !menu;
   if (!menu) {
-    mobileMenu.style.display = "none";
+    navMenu.style.display = "none";
     heroText.style.display = "block";
   } else {
-    mobileMenu.style.display = "block";
+    navMenu.style.display = "block";
     heroText.style.display = "none";
   }
+}
+
+function toggleResponsive() {
+  mobileMenuBtn.style.display = "none";
+  // heroImageMobile.style.display = "none";
+  // heroImageDesktop.style.display = "block";
+
+  // navMenu.style.display = "inline-block";
 }
